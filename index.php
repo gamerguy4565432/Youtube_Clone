dc60664670573f4b4d9d54127234046fea411fd5
gamerguy4565432 <JordanColeman540@gmail.com>
2023-03-21 03:10:34 -0600

    Initial Commit of Youtube Clone

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Youtube_Clone</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <div id="pageContainer">
+        <div id="mastHeadContainer">
+            <h1>Youtube Clone</h1>
+        </div>
+        <div id="sideNavContainer">
+        </div>
+        <div id="mainSectionContainer">
+        </div>
+    </div>
+</body>
+</html>
